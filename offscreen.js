0edf0372c00bc0902c8917e18cd40f363837aee5
pearlbijoy <pearlbijoy.com>
2026-07-16 00:13:22 +0530

    added mediapipe vision_bundle and wasm files, loaded HandLandMarker and fixed CSP

--- offscreen.js
+++ offscreen.js
@@ -1,3 +1,4 @@
+import { HandLandmarker, FilesetResolver } from "./vision_bundle.mjs";
 async function getCamera(){
     console.log("Asking for camera..");
     const videoFeed= await navigator.mediaDevices.getUserMedia({video:true});
@@ -6,3 +7,15 @@ async function getCamera(){
     console.log("Camera Active");
 }
 getCamera()
+
+async function loadHandLandmarker(){
+    const vision= await FilesetResolver.forVisionTasks("./wasm");
+    const handLandmarker = await HandLandmarker.createFromOptions(
+    vision,
+    {baseOptions: {modelAssetPath: "./hand_landmarker.task"},runningMode: "VIDEO",numHands: 2}
+    );
+    if(handLandmarker){
+        console.log("Landmarker object was created successfully.");
+    }
+}
+loadHandLandmarker()
